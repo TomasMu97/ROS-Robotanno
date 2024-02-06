ec469c34c458b48654a1c105bf42dfcf1b70ff18
Tomas Muñoz <129495947+TomasMu97@users.noreply.github.com>
2024-02-06 09:12:22 +0100

    Delete Github directory

--- Github/build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "moveit_core;moveit_ros_planning_interface;probot_msgs;roscpp;rospy".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "probot_g602_demo"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
--- Github/catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/tomas/probot_g602_ws/src/probot_g602/probot_g602_demo/catkin_ws/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
